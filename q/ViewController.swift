23c18237a7dd01dccda2b6c9495632ed182d4ae8
Jesse Shawl <jesse@jshawl.com>
2015-05-07 20:16:36 -0400

    add new xcode project

---
+++ q/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  q
+//
+//  Created by Jesse Shawl on 5/7/15.
+//  Copyright (c) 2015 Jesse Shawl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
