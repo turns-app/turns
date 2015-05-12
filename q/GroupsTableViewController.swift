425b18341c6130b795bc0054c777aadb82adc3d8
Jesse Shawl <jesse@jshawl.com>
2015-05-12 17:09:20 -0400

    user can remove groups

--- q/GroupsTableViewController.swift
+++ q/GroupsTableViewController.swift
@@ -61,17 +61,18 @@ class GroupsTableViewController: UITableViewController, UITableViewDataSource, U
     }
     */
 
-    /*
+   
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            groups?.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
