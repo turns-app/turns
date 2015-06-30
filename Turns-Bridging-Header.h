ae6bd3912c1c300bb0691c1f945545f13e744152
Jesse Shawl <jesse@jshawl.com>
2015-06-30 19:51:31 -0400

    import parse thank jesus

---
+++ Turns-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Turns-Bridging-Header.h
+//  Turns
+//
+//  Created by Jesse Shawl on 6/30/15.
+//  Copyright (c) 2015 Jesse Shawl. All rights reserved.
+//
+
+#ifndef Turns_Turns_Bridging_Header_h
+#define Turns_Turns_Bridging_Header_h
+
+#import <Parse/Parse.h>
+
+#endif
---
+++ bridgingHeader.m
@@ -0,0 +1,9 @@
+//
+//  bridgingHeader.m
+//  Turns
+//
+//  Created by Jesse Shawl on 6/30/15.
+//  Copyright (c) 2015 Jesse Shawl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
